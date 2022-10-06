5dad9a764fc1738d95bcf724bbae08ba9bd2b662
ZoRaToS <deladred.aledriand@gmail.com>
2022-10-06 14:48:34 +0300

    Add keywords: /ship, /get 20$

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,5 @@
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
-
-
 public class Main {
     public static void main(String[] args) throws Exception {
         var bots = new TelegramBotsApi(DefaultBotSession.class);
--- src/main/java/MyFirstBot.java
+++ src/main/java/MyFirstBot.java
@@ -1,6 +1,10 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,22 +12,45 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
 
-public class MyFirstBot extends TelegramLongPollingBot  {
+public class MyFirstBot extends TelegramLongPollingBot {
 
     Properties properties = new Properties();
 
-    private String botLogin ;
-    private String botToken ;
+    private String botLogin;
+    private String botToken;
 
     public MyFirstBot() throws IOException {
         //GEt token and login from comfiguration file
-        try(InputStream inputStream = Files.newInputStream(Paths.get("botconfig.ini"))) {
+        try (InputStream inputStream = Files.newInputStream(Paths.get("botconfig.ini"))) {
+//            inputStream.close();
             properties.load(inputStream);
             this.botLogin = properties.getProperty("login");
             this.botToken = properties.getProperty("token");
 
         }
     }
+
+
+    void sendMessage(long chatId, String text) throws Exception {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(text);
+
+        execute(message);
+
+    }
+
+    void sendPhoto(long chatId, String photoName) throws Exception {
+//        var name = new Random().nextInt(2);
+        var photo = getClass().getClassLoader().getResourceAsStream(photoName + ".jpg");
+
+        var message = new SendPhoto();
+        message.setChatId(chatId);
+        message.setPhoto(new InputFile(photo, "photo"));
+
+        execute(message);
+    }
+
 
     @Override
     public String getBotUsername() {
@@ -38,7 +65,26 @@ public class MyFirstBot extends TelegramLongPollingBot  {
     @Override
     public void onUpdateReceived(Update update) {
         String message = update.getMessage().getText();
+        var chatId = update.getMessage().getChatId();
         System.out.println(message);
+
+        try {
+            switch (message) {
+                case "/ship":
+                    sendPhoto(chatId, "ship");
+                    break;
+                case "/get 20$":
+                    sendPhoto(chatId, "$20");
+                    break;
+                default:
+                    sendMessage(chatId, "I don't understand");
+                    break;
+            }
+
+        } catch (Exception e) {
+
+        }
+
 
     }
 }
